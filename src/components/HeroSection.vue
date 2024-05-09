c3c50e7b378e9baf5028245794432c0910649c23
AbdulHameedi <sanilinity507@gmail.com>
2024-05-09 21:50:05 +0100

    adjustHero&project

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -27,19 +27,20 @@ export default defineComponent({
 </script>
 <template>
     <main class="heroVideo relative">
+        <!-- videoHero -->
         <div class="absolute top-0 bottom-0 right-0">
             <video ref="video" class="video" width="290" src="../assets/heroVideo.mp4" loop muted></video>
         </div>
         <!-- control button -->
-        <div class="z-20 absolute top-[50%] left-[50%] bg-white w-fit rounded-[50%] p-4 mx-auto" @click="togglePlay">
+        <div class="z-20 absolute top-[50%] left-[50%]  bg-white w-fit rounded-[50%] p-4 mx-auto" @click="togglePlay">
             <svg v-if="isPlaying" class="rounded text-[#2F2F2F]" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><path fill="currentColor" d="M14 19V5h4v14zm-8 0V5h4v14z"/></svg>
-            <svg v-else class="rounded text-[#2F2F2F]" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><path fill="currentColor" d="M8 19V5l11 7z"/></svg>
+            <svg v-else class="rounded text-[#2F2F2F] max-[425px]:w-[50px] max-[425px]:h-[50px]" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><path fill="currentColor" d="M8 19V5l11 7z"/></svg>
         </div>
         
-        <section class="p-8 md:px-24 py-10 absolute top-0 bottom-0">
+        <section class="p-4 md:px-24 py-10 absolute top-0 bottom-0">
             <NavBar />
             <div class="max-w-[90%] mb-8">
-                <p class="text-white font-extrabold text-8xl tracking-tighter">A motion designer with a <b class="text-[#F6DB06]">nack</b> for awesomeness</p>
+                <p class="text-white font-extrabold max-[443px]:text-3xl text-5xl md:text-8xl tracking-tighter">A motion designer with a <b class="text-[#F6DB06]">nack</b> for awesomeness</p>
                 <img class="zigzagImage" src="../assets/zigzagHero.png" alt="">
             </div>
         </section> 
@@ -64,7 +65,12 @@ export default defineComponent({
 .video{
     height: calc(100vh + 250px);
     width: 100%;
-    object-fit: cover;
+    object-fit: initial;
+}
+@media screen and (max-width: 425px){
+    .heroVideo,.video{
+        height: calc(100vh)
+    }
 }
 </style>
 
