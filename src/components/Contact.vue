3f7deb5d04ffe4a37bc7acfb39b3df509bcd327f
AbdulHameedi <sanilinity507@gmail.com>
2024-05-10 22:38:42 +0100

    contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,14 +12,15 @@ export default defineComponent({
 </script>
 <template>
     <section class="bg-[#FFFDED] px-4 md:px-24">
-        <div class="w-full bg-black p-4 flex gap-10 rounded-2xl">
-            <img src="../assets/azeem_pic.png" alt="azeem pic">
-            <div class="text-white">
-                <p>I am  Abdulazeem Akanji</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet incidunt facere atque delectus rem, voluptatum recusandae minus qui minima temporibus voluptate modi ex corporis cum hic quam similique quibusdam placeat quod. Quasi dolores itaque tempore debitis eum non, blanditiis repudiandae!</p>
+        <div class="w-full bg-black p-3 md:p-8 flex gap-5 md:gap-20 max-[1220px]:flex-col rounded-3xl items-center ">
+            <div class="overflow-hidden w-[35%] max-[1220px]:w-full h-[400px] rounded-3xl">
+                <img class="w-full h-full transition-all duration-300 hover:scale-110" src="/img/azeem_pic.png" alt="azeem pic">
+            </div>
+            <div class="text-white w-[65%] max-[1220px]:w-full pr-20 max-[1220px]:pr-0">
+                <p class="md:text-7xl tracking-tighter mb-5 text-4xl">I am <b class="text-[#F6DB06]">Abdulazeem Akanji</b></p>
+                <p class="leading-8 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet incidunt facere atque delectus rem, voluptatum recusandae minus qui minima temporibus voluptate modi ex corporis cum hic quam similique quibusdam placeat quod. Quasi dolores itaque tempore debitis eum non, blanditiis repudiandae!</p>
             </div>
         </div>
     </section>
 </template>
-
 
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -17,12 +17,12 @@ export default defineComponent({
 <main class="bg-[#FFFDED] px-4 pt-24 md:pt-24 md:px-24 pb-14">
     <p class="mb-14 font-bold text-3xl tracking-tighter">Projects</p>
     <section class="bg-[#FFFDED] grid grid-cols-2 gap-5 max-[1127px]:grid-cols-1">
-       <ProjectCard projectType="Deex Options" description="A Trading Platform" videoFile="../src/assets/heroVideo.mp4"/>
-       <ProjectCard projectType="Muslim Tech Expo" description="A Muslim Tech Community" videoFile="../src/assets/video_2.mp4"/>
-       <ProjectCard projectType="IVC by MSSN Lagos" description="Islamic Vacation Course" videoFile="../src/assets/heroVideo.mp4"/>
-       <ProjectCard projectType="Mission Telecom" description="A 501 C4 Non-Profit Foundation" videoFile="../src/assets/video_2.mp4"/>
-       <ProjectCard projectType="Elance" description="A Design Agency" videoFile="../src/assets/heroVideo.mp4"/>
-       <ProjectCard projectType="Random Stuffs" description="Just me playing around" videoFile="../src/assets/video_2.mp4"/>
+       <ProjectCard projectType="Deex Options" description="A Trading Platform" videoFile="/video/heroVideo.mp4"/>
+       <ProjectCard projectType="Muslim Tech Expo" description="A Muslim Tech Community" videoFile="/video/video_2.mp4"/>
+       <ProjectCard projectType="IVC by MSSN Lagos" description="Islamic Vacation Course" videoFile="/video/heroVideo.mp4"/>
+       <ProjectCard projectType="Mission Telecom" description="A 501 C4 Non-Profit Foundation" videoFile="/video/video_2.mp4"/>
+       <ProjectCard projectType="Elance" description="A Design Agency" videoFile="/video/heroVideo.mp4"/>
+       <ProjectCard projectType="Random Stuffs" description="Just me playing around" videoFile="/video/video_2.mp4"/>
     </section>
 </main>
 </template>
