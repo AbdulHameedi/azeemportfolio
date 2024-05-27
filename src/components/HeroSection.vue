c01cc1c1c61522ab10b6b2ceeda5210a3dc7f0a6
AbdulHameedi <sanilinity507@gmail.com>
2024-05-27 23:17:32 +0100

    videoadjust

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,20 +1,17 @@
 <template>
-    <main class="heroVideo relative">
-        <!-- videoHero -->
-        <div class="absolute top-0 bottom-0 right-0">
-            <video ref="video" class="video" src="https://res.cloudinary.com/doxshmcth/video/upload/v1/abdulazeemportfolio/heroVid_ug674z.mp4?_s=vp-2.0.2" autoplay loop muted></video>
-        </div>
-        <section class="p-4 md:px-[13%] py-10 absolute top-0 bottom-0">
+        <section class="p-4 md:px-[13%] py-10 bg-black pb-0 w-screen">
             <NavBar />
-            <!-- bodyHero -->
-            <div class="max-w-[90%] mb-8" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
+            <div class="max-w-[90%] mb-16" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
                 <p class="text-white font-extrabold text-7xl max-[950px]:text-4xl max-[398px]:text-3xl tracking-tighter">A motion designer with a <b class="text-[#F6DB06]">nack</b> for awesomeness</p>
                 <img class="max-[424px]:hidden" src="/img/zigzagHero.png" alt="zigzagHero">
                 <img  class="min-[425px]:hidden" src="/img/zigzagForm.png" alt="zigzagHero">
             </div>
+            <section class="flex flex-col items-center h-[370px]" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
+                <div class="bg-white w-[80%] flex flex-col items-center border border-[#F6DB06] rounded-sm" >
+                    <video ref="video" class="h-[370px] w-full video" src="https://res.cloudinary.com/doxshmcth/video/upload/v1/abdulazeemportfolio/heroVid_ug674z.mp4?_s=vp-2.0.2" autoplay loop muted></video>
+                </div>
+            </section>
         </section>
-    </main>
-    
 </template>
 <script>
 import { defineComponent } from 'vue';
@@ -30,12 +27,7 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-.heroVideo{
-    height: calc(100vh);
-}
 .video{
-    height: calc(100vh);
-    width: 100vw;
     object-fit: initial;
 }
 </style>
