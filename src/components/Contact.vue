277fc8f2328fd379d7bc826fc7749c1547cdfe1c
AbdulHameedi <sanilinity507@gmail.com>
2024-07-05 23:50:39 +0100

    edit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -11,25 +11,17 @@ export default defineComponent({
 })
 </script>
 <template>
-    <!-- <section class="px-[100px]">
-        <div class="bg-black rounded-[34px]">
-            <div>
-                <img src="/img/azeem_pic.png" alt="">
-            </div>
-        </div>
-    </section> -->
-    <!-- <div class="">
-        <section class="flex flex-col px-[100px]" id="about">
-            <div class="bg-black flex rounded-[34px] items-center py-[72px] gap-[80px] px-[57px]" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
-                <div class="overflow-hidden rounded-3xl">
-                    <img class="w-[354px] h-[401.45px] transition-all duration-300 hover:scale-110" src="/img/azeem_pic.png" alt="azeem pic">
+        <section class="sm:px-[100px] px-4 2xl:flex flex-col items-center" id="about" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
+                <div class="bg-black text-white p-[59px] rounded-[34px] flex items-center gap-16 2xl:w-[1269px]">
+                    <div class="h-[402.45px] w-[1460px] overflow-hidden rounded-[34px]">
+                        <img class="h-full w-full transition-all duration-300 hover:scale-110" src="/img/azeem_pic.png" alt="">
+                    </div>
+                    <div>
+                        <p class="text-[73.49px] mb-2 max-w-[600px] leading-[0.9]">I am <span class="font-[600] text-[#F6DB06]">Ayomide Akanji</span></p>
+                        <p class="text-[19.44px] font-[400] mb-8">or you can call me Abdulazeem. I am a motion designer with 3+ years of experience and a keen interest in story-telling and captivating motion related to various industries.  I am well-versed in ideating, illustrating, storyboarding, and executing ideas for the final piece.</p>
+                        <p class="text-[19.44px] font-[400]">I have had the pleasure of working with Proceipt UK,﻿  Obiex, One Liquidity, Teksight Edge and Enterscale. Feel free to contact me at me@ayomideakanji.com. You can download my resume here</p>
+                    </div>
                 </div>
-                <div class="text-white w-[75%] max-[1220px]:w-full pr-20 max-[1220px]:pr-0">
-                    <p class="md:text-6xl tracking-tighter mb-5 text-[73.49px]">I am <b class="text-[#F6DB06]">Abdulazeem Akanji</b></p>
-                    <p class="leading-8 text-[19.44px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet incidunt facere atque delectus rem, voluptatum recusandae minus qui minima temporibus voluptate modi ex corporis cum hic quam similique quibusdam placeat quod. Quasi dolores itaque tempore debitis eum non, blanditiis repudiandae!</p>
-                </div>
-            </div>
         </section>
-    </div> -->
 </template>
 
