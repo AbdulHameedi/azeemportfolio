185a8514e77f18ea3509222e6f9e1f1174df64bb
AbdulHameedi <sanilinity507@gmail.com>
2024-07-10 01:06:10 +0100

    edit

---
+++ src/components/Mooyi.vue
@@ -0,0 +1,30 @@
+<template>
+<ProjectPage 
+    imgHead="/img/mooyi.png" 
+    width="w-[265px]"
+    industry="Talent Booking"
+    typeProject="Booking platform"
+    duration="1 month (Contract)"
+    bgFrame="https://res.cloudinary.com/doxshmcth/video/upload/v1720478158/abdulazeemportfolio/MTE_2.0_VIDEO_ttqtia.mp4"
+    frameOne="/img/frame_1_mooyi.gif"
+/>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import ProjectPage from './ProjectPage.vue';
+
+export default defineComponent({
+    data(){
+        return{
+            
+        }
+    },
+    components: {
+        ProjectPage
+    }
+
+})
+</script>
+
+
+
--- src/components/ProjectPage.vue
+++ src/components/ProjectPage.vue
@@ -1,6 +1,7 @@
 <script>
 import { defineComponent } from 'vue';
 import NavBar from './NavBar.vue'
+import CursorEffect from './CursorEffect.vue'
 
 export default defineComponent({
     data(){
@@ -8,57 +9,52 @@ export default defineComponent({
 
         }
     },
-    components: {
-        NavBar
+    props:{
+        imgHead: String,
+        width: String,
+        industry: String,
+        typeProject: String,
+        bgFrame: String,    
+        duration: String,
+        frameOne: String,
+        frameTwo: String,
+        frameThree: String,
+        frameFour: String,
+        frameFive: String
     },
-    mounted(){
-        const cursorDot = document.querySelector("[data-cursor-dot]");
-        const cursorOutline = document.querySelector("[data-cursor-outline]")
-        window.addEventListener("mousemove", function(e){
-            const posX = e.clientX;
-            const posY = e.clientY;
-
-            cursorDot.style.left = `${posX}px`;
-            cursorDot.style.top = `${posY}px`;
-
-            cursorOutline.animate({
-                left: `${posX}px`,
-                top: `${posY}px`
-            }, {duration: 800,fill:"forwards"});
-        })
-    }
+    components: {
+        NavBar,
+        CursorEffect
+    },
 })
 </script>
 <template>
-    <div class="cursor-dot" data-cursor-dot></div>
-    <div class="cursor-outline" data-cursor-outline></div>
+    <CursorEffect />
     <section>
         <section class="py-10 bg-[#2F2F2F] w-full px-4 sm:px-[100px]">
             <NavBar />
             <div class="2xl:flex flex-col items-center">    
                 <div>
                     <div class="relative mb-[4rem]" data-aos="fade-up" data-aos-duration="300" data-aos-offset="200"  data-aos-easing="ease-in-out">
-                        <video ref="video" class="video w-full rounded-[40px]" src="https://res.cloudinary.com/doxshmcth/video/upload/v1720478158/abdulazeemportfolio/MTE_2.0_VIDEO_ttqtia.mp4" autoplay loop muted></video>
+                        <video ref="video" class="video w-full rounded-[40px]" :src="bgFrame" autoplay loop muted></video>
                         <svg class="absolute left-[44px] bottom-[43px]" width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.5 0C11.4169 0 0 11.4167 0 25.5C0 39.5833 11.4169 51 25.5 51C39.5831 51 51 39.5833 51 25.5C51 11.4167 39.5831 0 25.5 0ZM34.3134 26.8517L21.5634 34.8204C21.3054 34.9815 21.0121 35.0625 20.7188 35.0625C20.453 35.0625 20.1868 34.9964 19.946 34.8625C19.4394 34.5816 19.125 34.0485 19.125 33.4688V17.5312C19.125 16.9515 19.4394 16.4184 19.946 16.1375C20.4526 15.855 21.0721 15.8722 21.5634 16.1796L34.3134 24.1483C34.7792 24.4402 35.0625 24.9507 35.0625 25.5C35.0625 26.0493 34.7792 26.5599 34.3134 26.8517Z" fill="white"/></svg>
                     </div>
                     <div>
                         <div class="mb-[31px]">
-                            <!-- <img src="/img/logo.png" alt="logo" for-div{flex items-center gap-[2rem] }>
-                            <p class="text-white font-[600] tracking-tightest text-[40px]">Playground</p> -->
-                            <img src="/img/mte.png" alt="mte">
+                            <img :class="[width]" :src="imgHead" alt="img-head">
                         </div>
                         <div class="flex items-center text-[16px] gap-[7rem]">
                             <div>
                                 <p class="gray-50">Industry</p>
-                                <p class="text-white">NGO</p>
+                                <p class="text-white">{{industry}}</p>
                             </div>
                             <div>
                                 <p class="gray-50">Project type</p>
-                                <p class="text-white">Event Campaign</p>
+                                <p class="text-white">{{ typeProject }}</p>
                             </div>
                             <div>
                                 <p class="gray-50">Project duration</p>
-                                <p class="text-white">1 month</p>
+                                <p class="text-white">{{ duration }}</p>
                             </div>
                         </div>
                     </div>
@@ -67,17 +63,25 @@ export default defineComponent({
         </section>
         <section class="px-4 sm:px-[100px] py-[100px] bg-[#FFFDED]">
             <div class="2xl:flex flex-col items-center">
-                <p class="text-[58.35px] font-[600] text-[#2F2F2F] tracking-tightest">Execution</p>
-                <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px]">
+                <p class="text-[58.35px] font-[600] text-[#2F2F2F] tracking-tightest mb-[32px]">Execution</p>
+                <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px] mb-[32px]">
                     <p class="mb-[38px] text-[32px] font-[500] tracking-tightest">Other animations</p>
                     <div class="flex gap-[23px] mb-[24px]">
-                        <img class="w-[65%] rounded-[15px]" src="https://res.cloudinary.com/doxshmcth/image/upload/v1720476317/abdulazeemportfolio/BTS_ELLANCE_11_xqaoca.gif" alt="frame_1">
-                        <img class="w-[35%] rounded-[15px]" src="/img/frame_2_mte.gif" alt="">
+                        <img class="w-[65%] rounded-[15px]" :src="frameOne" alt="frame_1">
+                        <img class="w-[35%] rounded-[15px]" src="/img/frame_2_mte.gif" alt="frame_2">
                     </div>
                     <div class="flex gap-5">
                         <img class="w-[40%] rounded-[15px]" src="https://res.cloudinary.com/doxshmcth/image/upload/v1720477236/abdulazeemportfolio/BTS_ELLANCE_12_t5xpce.gif" alt="frame_3">
                         <img class="w-[60%] rounded-[15px]" src="/img/frame_4_mte.gif" alt="frame_4">
                     </div>
+                </div>
+                <!--  -->
+                <div class="bg-white rounded-[35.54px] p-4 sm:p-[33.87px] sm:px-[63px] lg:gap-4 gap-10 grid lg:grid-cols-2 grid-col-1 items-center">
+                    <div>
+                        <p class="font-[500] text-[33.32px]">Animating</p>
+                        <p class="max-w-[350px] font-[400] text-[20.82px] leading-[28.11px]">Here we are talking about the process of animating it and bla bla bla </p>
+                    </div>
+                    <img class="rounded-[15px] w-full" src="/img/frame_5_mte.gif" alt="frame_5">
                 </div>
             </div>
         </section>
@@ -88,34 +92,7 @@ export default defineComponent({
     font-family: "Work Sans", sans-serif;
     scroll-behavior: smooth;
 }
-body{
-    cursor: none;
-}
 .gray-50{
     color: hsl(0, 0%, 50%);
-}
-.cursor-dot{
-    width: 5px;
-    height: 5px;
-    background-color: black;
-}
-.cursor-outline{
-    width: 30px;
-    height: 30px;
-    border: 2px solid #F6DB06;
-}
-.cursor-dot, .cursor-outline{
-    position: fixed;
-    top: -10px;
-    left: -10px;
-    transform: translate(-50%,-50%);
-    border-radius: 50%;
-    z-index: 999;
-    pointer-events: none;
-}
-@media screen and (max-width:425px){
-    body{
-        cursor: pointer;
-    }
 }
 </style>
