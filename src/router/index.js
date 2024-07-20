ca489f7a61cc3da747df2e328289f4aa356cd506
AbdulHameedi <sanilinity507@gmail.com>
2024-07-21 00:50:20 +0100

    edit

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  base: '/https://azeemportfolio-psi.vercel.app/',
+  base: '/https://ayomideakanji.com/',
   routes: [
     {
       path: '/',
