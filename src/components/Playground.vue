71c8b85c9643681201dd1e44d998b0ca38537b90
AbdulHameedi <sanilinity507@gmail.com>
2024-09-06 23:43:16 +0100

    edit

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -53,10 +53,10 @@ export default defineComponent({
                 </div>
             </div>
         </section>
-        <section class="px-4 sm:px-[100px] py-[100px] bg-[#FFFDED] ">
+        <section class="px-4 sm:px-[100px] py-[100px] bg-[#FFFDED]">
             <div class="max-w-[1536px] mx-auto">
                 <p class="md:text-[58.35px] text-[40.32px] font-[600] text-[#2F2F2F] tracking-tightest mb-[32px]">Execution</p>
-                <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px] mb-[32px]">
+                <!-- <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px] mb-[32px]">
                     <p class="mb-[38px] md:text-[32px] text-[33.32px] font-[500] tracking-tightest">Other animations</p>
                     <div class="flex flex-col md:flex-row md:gap-[23px] mb-[24px]">
                         <video ref="video" class="md:w-[65%] rounded-[7px] md:rounded-[15px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1724802325/Send_App_Flutterwave_Campaign_SIBLINGS_PERSPECTIVE1_uwydke.mp4" autoplay loop muted></video>
@@ -66,9 +66,9 @@ export default defineComponent({
                         <video ref="video" class="md:w-[44%] rounded-[7px] md:rounded-[15px] mb-6 md:mb-0" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725526885/7_THE_WEALTHY_ISLET_VIdeEO_byjf9j.mp4" autoplay loop muted></video>
                         <video ref="video" class="md:w-[56%] rounded-[7px] md:rounded-[15px] mb-6 md:mb-0" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725491425/0_TECNO_VAMON_30_X_ATR_mgtj0t.mp4" autoplay loop muted></video>
                     </div>
-                </div>
+                </div> -->
                 <!--  -->
-                <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px] mb-[32px]">
+                <!-- <div class="bg-white rounded-[35.54px] p-4 sm:p-[63px] mb-[32px]">
                     <div class="flex flex-col md:flex-row md:gap-[23px] mb-[24px]">
                         <video ref="video" class="md:w-[65%] rounded-[7px] md:rounded-[15px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725577319/8_EHINGBETI_SOCIAL_ehjtro.mp4" autoplay loop muted></video>
                         <video ref="video" class="md:w-[35%] rounded-[7px] md:rounded-[15px]" src="https://res.cloudinary.com/doxshmcth/video/upload/v1720304106/abdulazeemportfolio/MTE_preview_knjq7g.mp4" autoplay loop muted></video>
@@ -77,8 +77,16 @@ export default defineComponent({
                         <video ref="video" class="md:w-[44%] rounded-[7px] md:rounded-[15px] mb-6 md:mb-0" src="https://res.cloudinary.com/doxshmcth/video/upload/v1721382186/abdulazeemportfolio/Ayomide_Akanji_Reel_zkn3ub.mp4" autoplay loop muted></video>
                         <video ref="video" class="md:w-[56%] rounded-[7px] md:rounded-[15px] mb-6 md:mb-0" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725491425/0_TECNO_VAMON_30_X_ATR_mgtj0t.mp4" autoplay loop muted></video>
                     </div>
-                </div>
+                </div> -->
                 <!--  -->
+                <div class="bg-white p-4 rounded-[35.54px] sm:p-[63px] mb-[32px]">
+                    <div class="relative">
+                        <video ref="video" class="object-cover h-[700px] w-full rounded-[15px] md:rounded-[40px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725661362/00_Karafin_Tornado_Qatar_l4ljbx.mp4" autoplay loop muted></video>
+                        <a href="https://res.cloudinary.com/dt7jocfde/video/upload/v1725661362/00_Karafin_Tornado_Qatar_l4ljbx.mp4">
+                            <svg class="absolute left-[24px] md:left-[44px] bottom-[23px] md:bottom-[43px]" width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.5 0C11.4169 0 0 11.4167 0 25.5C0 39.5833 11.4169 51 25.5 51C39.5831 51 51 39.5833 51 25.5C51 11.4167 39.5831 0 25.5 0ZM34.3134 26.8517L21.5634 34.8204C21.3054 34.9815 21.0121 35.0625 20.7188 35.0625C20.453 35.0625 20.1868 34.9964 19.946 34.8625C19.4394 34.5816 19.125 34.0485 19.125 33.4688V17.5312C19.125 16.9515 19.4394 16.4184 19.946 16.1375C20.4526 15.855 21.0721 15.8722 21.5634 16.1796L34.3134 24.1483C34.7792 24.4402 35.0625 24.9507 35.0625 25.5C35.0625 26.0493 34.7792 26.5599 34.3134 26.8517Z" fill="white"/></svg>
+                        </a>
+                    </div>
+                </div>
                 <div class="bg-white p-4 rounded-[35.54px] sm:p-[63px] mb-[32px]">
                     <div class="relative">
                         <video ref="video" class="video w-full rounded-[15px] md:rounded-[40px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725491425/0_TECNO_VAMON_30_X_ATR_mgtj0t.mp4" autoplay loop muted></video>
@@ -87,21 +95,21 @@ export default defineComponent({
                         </a>
                     </div>
                 </div>
-            </div>
-        </section>
-        <section class="bg-[#2F2F2F] sm:p-[100px] p-4">
-            <div class="max-w-[1536px] mx-auto">
-                <div class="sm:grid-cols-2 grid grid-col-1 sm:gap-0 gap-6 md:items-center border-b pb-6">
-                    <p class="text-[17.22px] gray-50 font-[400] tracking-tightest">Closing Thought</p>
-                    <p class="text-white leading-[34.44px] text-[19px] font-[400] tracking-tightest">Within two enchanted weeks, I wove a bundle of animations for Ellance Agency. As their motion sorcerer, I breathed life into pixels, casting spells that now dance across their website.</p>
+                <div class="bg-white p-4 rounded-[35.54px] sm:p-[63px] mb-[32px]">
+                    <div class="relative">
+                        <video ref="video" class="video w-full rounded-[15px] md:rounded-[40px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1724802325/Send_App_Flutterwave_Campaign_SIBLINGS_PERSPECTIVE1_uwydke.mp4" autoplay loop muted></video>
+                        <a href="https://res.cloudinary.com/dt7jocfde/video/upload/v1724802325/Send_App_Flutterwave_Campaign_SIBLINGS_PERSPECTIVE1_uwydke.mp4">
+                            <svg class="absolute left-[24px] md:left-[44px] bottom-[23px] md:bottom-[43px]" width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.5 0C11.4169 0 0 11.4167 0 25.5C0 39.5833 11.4169 51 25.5 51C39.5831 51 51 39.5833 51 25.5C51 11.4167 39.5831 0 25.5 0ZM34.3134 26.8517L21.5634 34.8204C21.3054 34.9815 21.0121 35.0625 20.7188 35.0625C20.453 35.0625 20.1868 34.9964 19.946 34.8625C19.4394 34.5816 19.125 34.0485 19.125 33.4688V17.5312C19.125 16.9515 19.4394 16.4184 19.946 16.1375C20.4526 15.855 21.0721 15.8722 21.5634 16.1796L34.3134 24.1483C34.7792 24.4402 35.0625 24.9507 35.0625 25.5C35.0625 26.0493 34.7792 26.5599 34.3134 26.8517Z" fill="white"/></svg>
+                        </a>
+                    </div>
                 </div>
-                <div class="sm:grid-cols-2 grid grid-col-1 sm:gap-0 gap-6 items-center border-b py-6">
-                    <p class="text-[17.22px] gray-50 font-[400] tracking-tightest">Tools</p>
-                    <p class="text-white text-[19px] font-[400] tracking-tightest">After effects | Figma | Illustrator</p>
-                </div>
-                <div class="sm:grid-cols-2 grid grid-col-1 sm:gap-0 gap-6 items-center border-b py-6">
-                    <p class="text-[17.22px] gray-50 font-[400] tracking-tightest">Credits</p>
-                    <p class="text-white text-[19px] font-[400] tracking-tightest">Ellance</p>
+                <div class="bg-white p-4 rounded-[35.54px] sm:p-[63px] mb-[32px]">
+                    <div class="relative">
+                        <video ref="video" class="object-cover h-[700px] w-full rounded-[15px] md:rounded-[40px]" src="https://res.cloudinary.com/dt7jocfde/video/upload/v1725662389/2_EAGLELINE_CONSULT_FINAL_RENDER_111_ddsqid.mp4" autoplay loop muted></video>
+                        <a href="https://res.cloudinary.com/dt7jocfde/video/upload/v1725662389/2_EAGLELINE_CONSULT_FINAL_RENDER_111_ddsqid.mp4">
+                            <svg class="absolute left-[24px] md:left-[44px] bottom-[23px] md:bottom-[43px]" width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.5 0C11.4169 0 0 11.4167 0 25.5C0 39.5833 11.4169 51 25.5 51C39.5831 51 51 39.5833 51 25.5C51 11.4167 39.5831 0 25.5 0ZM34.3134 26.8517L21.5634 34.8204C21.3054 34.9815 21.0121 35.0625 20.7188 35.0625C20.453 35.0625 20.1868 34.9964 19.946 34.8625C19.4394 34.5816 19.125 34.0485 19.125 33.4688V17.5312C19.125 16.9515 19.4394 16.4184 19.946 16.1375C20.4526 15.855 21.0721 15.8722 21.5634 16.1796L34.3134 24.1483C34.7792 24.4402 35.0625 24.9507 35.0625 25.5C35.0625 26.0493 34.7792 26.5599 34.3134 26.8517Z" fill="white"/></svg>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
